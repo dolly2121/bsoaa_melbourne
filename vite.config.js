bcb023f216f5fca93537577d5e93d0d86b70b13f
Dolly <dsah0008@student.monash.edu>
2026-04-29 10:50:44 +1000

    reverting the domain

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/bsoaa_melbourne/',
-// })
-
-/**formal domain**/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/bsoaa_melbourne/',
 })
+
+// /**formal domain**/
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+// })
